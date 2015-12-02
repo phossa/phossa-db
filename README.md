092e95167e319f62ac6a8defb313583bc8c5323b
Hong Zhang <phossa@126.com>
2015-12-02 11:43:48 +0800

    init commit
    init commit

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-# phossa-db
-DB connection, management, statistics package for Phossa project
+# Introduction
+Phossa is a lightweight PHP framework. Phossa-db is db connection, management,
+statistics package.
 
-# bingo
+# Features
+
+- Support PHP 5.4+
+
+- PHP7 ready for return type declarations and argument type declarations.
+
+# Version
+1.0.0
+
+# Dependencies
+PHP >= 5.4.0 and Phossa-shared 1.*
---
+++ src/Phossa/Db/Message/Message.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Phossa Project
+ *
+ * @see         http://www.phossa.com/
+ * @copyright   Copyright (c) 2015 phossa.com
+ * @license     http://mit-license.org/ MIT License
+ */
+/*# declare(strict_types=1); */
+
+namespace Phossa\Db\Message;
+
+use Phossa\Shared\Message\MessageAbstract;
+
+/**
+ * Message class for Phossa\Db
+ *
+ * @package \Phossa\Db
+ * @author  Hong Zhang <phossa@126.com>
+ * @version 1.0.0
+ * @since   1.0.0 added
+ */
+class Message extends MessageAbstract
+{
+    /**#@+
+     * @var   int
+     * @type  int
+     */
+
+    /**#@-*/
+
+    /**
+     * {@inheritdoc}
+     */
+    protected static $messages = [
+    ];
+}
